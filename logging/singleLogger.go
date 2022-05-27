4518087384d9ce0c5d5ee388c131ca5330e315a1
AlinKwon <alin.kwon@kakao.com>
2022-05-27 10:37:31 +0900

    single instance logger2

--- logging/singleLogger.go
+++ logging/singleLogger.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"sync"
+
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var logger *log.Logger
@@ -11,7 +14,20 @@ var once sync.Once
 
 func GetLogger() *log.Logger {
 	once.Do(func() {
-		logger = log.New(os.Stdout, " ", 5)
+		if err := os.MkdirAll("logs", os.ModePerm); err != nil {
+			panic(err)
+		}
+		f := &lumberjack.Logger{
+			Filename:   "logs/log.log",
+			MaxSize:    1,  // megabytes after which new file is created
+			MaxBackups: 3,  // number of backups
+			MaxAge:     28, //days
+		}
+
+		mr := io.MultiWriter(f, os.Stdout)
+
+		logger = log.New(mr, " ", 5)
+		logger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	})
 	return logger
 }
