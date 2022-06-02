8fd60f6d3a8dce857988e7c01947e37b54cd2d3b
AlinKwon <alin.kwon@kakao.com>
2022-06-02 14:34:04 +0900

    logger pass

--- windows-service/internal/singleInstanceLogger.go
+++ windows-service/internal/singleInstanceLogger.go
@@ -1,10 +1,12 @@
 package internal
 
 import (
-	"fmt"
 	"io"
 	"os"
+	"path"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"sync"
 
 	"go.uber.org/zap"
@@ -15,18 +17,44 @@ import (
 var singleLogger *zap.SugaredLogger
 var once sync.Once
 
+func userHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
+}
+
+func AbsPathify(inPath string) string {
+	if inPath == "$HOME" || strings.HasPrefix(inPath, "$HOME"+string(os.PathSeparator)) {
+		inPath = userHomeDir() + inPath[5:]
+	}
+
+	inPath = os.ExpandEnv(inPath)
+
+	if filepath.IsAbs(inPath) {
+		return filepath.Clean(inPath)
+	}
+
+	p, err := filepath.Abs(inPath)
+	if err == nil {
+		return filepath.Clean(p)
+	}
+	return ""
+}
+
 func GetLogger() *zap.SugaredLogger {
 	once.Do(func() {
-		ex, err := os.Executable()
-		if err != nil {
+		pwd := AbsPathify("./logs")
+		if err := os.MkdirAll(pwd, os.ModePerm); err != nil {
 			panic(err)
 		}
-		path := fmt.Sprintf("%s/logs", filepath.Dir(ex))
-		logFile := fmt.Sprintf("%s/log.log", path)
 
-		if err := os.MkdirAll(path, os.ModePerm); err != nil {
-			panic(err)
-		}
+		logFile := path.Join(pwd, "scrape-wsvc.log")
+
 		f := &lumberjack.Logger{
 			Filename:   logFile,
 			MaxSize:    1,  // megabytes after which new file is created
@@ -34,12 +62,14 @@ func GetLogger() *zap.SugaredLogger {
 			MaxAge:     28, //days
 		}
 
-		mr := io.MultiWriter(f, os.Stdout)
+		singleMultiWriter = io.MultiWriter(f, os.Stdout)
 
-		w := zapcore.AddSync(mr)
+		w := zapcore.AddSync(singleMultiWriter)
+		config := zap.NewProductionEncoderConfig()
+		config.EncodeTime = zapcore.ISO8601TimeEncoder
 
 		core := zapcore.NewCore(
-			zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+			zapcore.NewJSONEncoder(config),
 			w,
 			zap.DebugLevel,
 		)
